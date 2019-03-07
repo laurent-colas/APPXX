5eb5a97a6b3a7f13f0fae749d79a1d4e78a1b778
Joel Gaudreault <joel.gaudreault@usherbrooke.ca>
2019-03-07 18:16:58 -0500

    Ajout de l'initialisation et du spi master

--- S5/APP5/S5e_APP6/includes/SPI_Config.h
+++ S5/APP5/S5e_APP6/includes/SPI_Config.h
@@ -10,23 +10,25 @@
 
 #include <csl_mcbsp.h>
 
-MCBSP_Config MCBSP0_SPI_Cfg;
+MCBSP_Config MCBSP0_SPI_Cfg_Init;
 
-MCBSP_Config MCBSP0_SPI_Cfg = {
+MCBSP_Config MCBSP0_SPI_Cfg_Master;
+
+MCBSP_Config MCBSP0_SPI_Cfg_Init = {
         MCBSP_FMKS(SPCR, FREE, NO)              |
         MCBSP_FMKS(SPCR, SOFT, NO)              |
         MCBSP_FMKS(SPCR, FRST, YES)             |
-        MCBSP_FMKS(SPCR, GRST, YES)             |
+        MCBSP_FMKS(SPCR, GRST, 1)               | // …tape 3
         MCBSP_FMKS(SPCR, XINTM, XRDY)           |
         MCBSP_FMKS(SPCR, XSYNCERR, NO)          |
-        MCBSP_FMKS(SPCR, XRST, YES)             |
+        MCBSP_FMKS(SPCR, XRST, YES)             | // …tape 1
         MCBSP_FMKS(SPCR, DLB, OFF)              |
         MCBSP_FMKS(SPCR, RJUST, RZF)            |
-        MCBSP_FMKS(SPCR, CLKSTP, DISABLE)       |
+        MCBSP_FMKS(SPCR, CLKSTP, NODELAY)       | // …tape 2
         MCBSP_FMKS(SPCR, DXENA, OFF)            |
         MCBSP_FMKS(SPCR, RINTM, RRDY)           |
         MCBSP_FMKS(SPCR, RSYNCERR, NO)          |
-        MCBSP_FMKS(SPCR, RRST, YES),
+        MCBSP_FMKS(SPCR, RRST, YES),              // …tape 1
 
         MCBSP_FMKS(RCR, RPHASE, SINGLE)         |
         MCBSP_FMKS(RCR, RFRLEN2, DEFAULT)       |
@@ -50,8 +52,70 @@ MCBSP_Config MCBSP0_SPI_Cfg = {
 
         MCBSP_FMKS(SRGR, GSYNC, DEFAULT)        |
         MCBSP_FMKS(SRGR, CLKSP, DEFAULT)        |
-        MCBSP_FMKS(SRGR, CLKSM, DEFAULT)        |
+        MCBSP_FMKS(SRGR, CLKSM, DEFAULT)        | // Clock interne par dÈfault
         MCBSP_FMKS(SRGR, FSGM, DEFAULT)         |
+        MCBSP_FMKS(SRGR, FPER, DEFAULT)         |
+        MCBSP_FMKS(SRGR, FWID, DEFAULT)         |
+        MCBSP_FMKS(SRGR, CLKGDV, DEFAULT),        // Division de 225 Mhz -> vitesse maximal de l'uart
+
+        MCBSP_MCR_DEFAULT,
+        MCBSP_RCER_DEFAULT,
+        MCBSP_XCER_DEFAULT,
+
+        MCBSP_FMKS(PCR, XIOEN, SP)              |
+        MCBSP_FMKS(PCR, RIOEN, SP)              |
+        MCBSP_FMKS(PCR, FSXM, EXTERNAL)         |
+        MCBSP_FMKS(PCR, FSRM, EXTERNAL)         |
+        MCBSP_FMKS(PCR, CLKXM, OUTPUT)          |
+        MCBSP_FMKS(PCR, CLKRM, INPUT)           |
+        MCBSP_FMKS(PCR, CLKSSTAT, DEFAULT)      |
+        MCBSP_FMKS(PCR, DXSTAT, DEFAULT)        |
+        MCBSP_FMKS(PCR, FSXP, ACTIVEHIGH)       |
+        MCBSP_FMKS(PCR, FSRP, ACTIVEHIGH)       |
+        MCBSP_FMKS(PCR, CLKXP, FALLING)         |
+        MCBSP_FMKS(PCR, CLKRP, RISING)
+    };
+
+MCBSP_Config MCBSP0_SPI_Cfg_Master = {
+        MCBSP_FMKS(SPCR, FREE, NO)              |
+        MCBSP_FMKS(SPCR, SOFT, NO)              |
+        MCBSP_FMKS(SPCR, FRST, YES)             |
+        MCBSP_FMKS(SPCR, GRST, YES)             |
+        MCBSP_FMKS(SPCR, XINTM, XRDY)           |
+        MCBSP_FMKS(SPCR, XSYNCERR, NO)          |
+        MCBSP_FMKS(SPCR, XRST, YES)             |
+        MCBSP_FMKS(SPCR, DLB, OFF)              |
+        MCBSP_FMKS(SPCR, RJUST, RZF)            |
+        MCBSP_FMKS(SPCR, CLKSTP, DISABLE)       |
+        MCBSP_FMKS(SPCR, DXENA, OFF)            |
+        MCBSP_FMKS(SPCR, RINTM, RRDY)           |
+        MCBSP_FMKS(SPCR, RSYNCERR, NO)          |
+        MCBSP_FMKS(SPCR, RRST, YES),
+
+        MCBSP_FMKS(RCR, RPHASE, SINGLE)         |
+        MCBSP_FMKS(RCR, RFRLEN2, DEFAULT)       |
+        MCBSP_FMKS(RCR, RWDLEN2, DEFAULT)       |
+        MCBSP_FMKS(RCR, RCOMPAND, MSB)          |
+        MCBSP_FMKS(RCR, RFIG, NO)               |
+        MCBSP_FMKS(RCR, RDATDLY, 1BIT)          | // Pour permettre d'ini escalve avant le dÈbut de transmission
+        MCBSP_FMKS(RCR, RFRLEN1, OF(0))         | // This changes to 1 FRAME
+        MCBSP_FMKS(RCR, RWDLEN1, 32BIT)         | // This changes to 32 bits per frame
+        MCBSP_FMKS(RCR, RWDREVRS, DISABLE),
+
+        MCBSP_FMKS(XCR, XPHASE, SINGLE)         |
+        MCBSP_FMKS(XCR, XFRLEN2, DEFAULT)       |
+        MCBSP_FMKS(XCR, XWDLEN2, DEFAULT)       |
+        MCBSP_FMKS(XCR, XCOMPAND, MSB)          |
+        MCBSP_FMKS(XCR, XFIG, NO)               |
+        MCBSP_FMKS(XCR, XDATDLY, 1BIT)          | // Pour permettre d'ini escalve avant le dÈbut de transmission
+        MCBSP_FMKS(XCR, XFRLEN1, OF(0))         | // This changes to 1 FRAME
+        MCBSP_FMKS(XCR, XWDLEN1, 32BIT)         | // This changes to 32 bits per frame
+        MCBSP_FMKS(XCR, XWDREVRS, DISABLE),
+
+        MCBSP_FMKS(SRGR, GSYNC, DEFAULT)        |
+        MCBSP_FMKS(SRGR, CLKSP, DEFAULT)        |
+        MCBSP_FMKS(SRGR, CLKSM, DEFAULT)        |
+        MCBSP_FMKS(SRGR, FSGM, DXR2XSR)         | // FSGM = 0
         MCBSP_FMKS(SRGR, FPER, DEFAULT)         |
         MCBSP_FMKS(SRGR, FWID, DEFAULT)         |
         MCBSP_FMKS(SRGR, CLKGDV, DEFAULT),
@@ -62,7 +126,7 @@ MCBSP_Config MCBSP0_SPI_Cfg = {
 
         MCBSP_FMKS(PCR, XIOEN, SP)              |
         MCBSP_FMKS(PCR, RIOEN, SP)              |
-        MCBSP_FMKS(PCR, FSXM, EXTERNAL)         |
+        MCBSP_FMKS(PCR, FSXM, INTERNAL)         | //
         MCBSP_FMKS(PCR, FSRM, EXTERNAL)         |
         MCBSP_FMKS(PCR, CLKXM, OUTPUT)          | // SPI master dicte la clock
         MCBSP_FMKS(PCR, CLKRM, INPUT)           |
@@ -74,10 +138,71 @@ MCBSP_Config MCBSP0_SPI_Cfg = {
         MCBSP_FMKS(PCR, CLKRP, RISING)
     };
 
+/*MCBSP_Config MCBSP0_SPI_Cfg = {
+        MCBSP_FMKS(SPCR, FREE, NO)              |
+        MCBSP_FMKS(SPCR, SOFT, NO)              |
+        MCBSP_FMKS(SPCR, FRST, YES)             | // FSG est g√©n√©r√© par le sample-rate generator (Pas de sortie car le SRG est tenu en reset)
+        MCBSP_FMKS(SPCR, GRST, YES)             | // Le sample rate generator est tenu en reset
+        MCBSP_FMKS(SPCR, XINTM, XRDY)           | // Mode d'interruption de transmission
+        MCBSP_FMKS(SPCR, XSYNCERR, NO)          | // Erreur de synchronisation de transmission
+        MCBSP_FMKS(SPCR, XRST, YES)             | // Serial Port Transmitter est d√©sactiv√© et tenu en reset
+        MCBSP_FMKS(SPCR, DLB, OFF)              | // Le mode Digital Loop-back est d√©sactiv√©
+        MCBSP_FMKS(SPCR, RJUST, RZF)            | // Receive sign extension and justification mode bit
+        MCBSP_FMKS(SPCR, CLKSTP, DELAY)         | // Clock stop mode bit (DELAY = clock starts with delay)
+        MCBSP_FMKS(SPCR, DXENA, OFF)            | //
+        MCBSP_FMKS(SPCR, RINTM, RRDY)           | //
+        MCBSP_FMKS(SPCR, RSYNCERR, NO)          | //
+        MCBSP_FMKS(SPCR, RRST, YES),              //
 
-hMcbsp = MCBSP_open(MCBSP_DEV0,MCBSP_OPEN_RESET);
+        MCBSP_FMKS(RCR, RPHASE, SINGLE)         | // Single phase frame
+        MCBSP_FMKS(RCR, RFRLEN2, DEFAULT)       |
+        MCBSP_FMKS(RCR, RWDLEN2, DEFAULT)       |
+        MCBSP_FMKS(RCR, RCOMPAND, MSB)          |
+        MCBSP_FMKS(RCR, RFIG, NO)               |
+        MCBSP_FMKS(RCR, RDATDLY, 1BIT)          | // 1 bit delay
+        MCBSP_FMKS(RCR, RFRLEN1, OF(0))         | // 1 FRAME
+        MCBSP_FMKS(RCR, RWDLEN1, 16BIT)         | // 16 bits per frame
+        MCBSP_FMKS(RCR, RWDREVRS, DISABLE),
 
-MCBSP_config(hMcbsp,&MCBSP0_SPI_Cfg);
+        MCBSP_FMKS(XCR, XPHASE, SINGLE)         | // 1 Phase
+        MCBSP_FMKS(XCR, XFRLEN2, DEFAULT)       |
+        MCBSP_FMKS(XCR, XWDLEN2, DEFAULT)       |
+        MCBSP_FMKS(XCR, XCOMPAND, MSB)          |
+        MCBSP_FMKS(XCR, XFIG, NO)               |
+        MCBSP_FMKS(XCR, XDATDLY, 1BIT)          | // 1 bit delay
+        MCBSP_FMKS(XCR, XFRLEN1, OF(0))         | // This changes to 1 FRAME
+        MCBSP_FMKS(XCR, XWDLEN1, 16BIT)         | // This changes to 16 bits per frame
+        MCBSP_FMKS(XCR, XWDREVRS, DISABLE),
+
+        MCBSP_FMKS(SRGR, GSYNC, DEFAULT)        |
+        MCBSP_FMKS(SRGR, CLKSP, DEFAULT)        | // Sans int√©r√™t : Seulement utilis√© avec horloge externe
+        MCBSP_FMKS(SRGR, CLKSM, INTERNAL)       | // Input clock source mode (INTERNAL (1) = Sample-rate generator clock derived from the CPU clock)
+        MCBSP_FMKS(SRGR, FSGM, DEFAULT)         | // FSX est conduit par le Sample rate generator
+        MCBSP_FMKS(SRGR, FPER, DEFAULT)         | // Largeur de frame sync pulse
+        MCBSP_FMKS(SRGR, FWID, DEFAULT)         | // Trame de 16 bits
+        MCBSP_FMKS(SRGR, CLKGDV, OF(0x32)),       // Clock divide down (64, Number of input clocks per data bit clock, to generate the required sample-rate generator clock frequency) Fe = 439453,125 Hz
+
+        MCBSP_MCR_DEFAULT,
+        MCBSP_RCER_DEFAULT,
+        MCBSP_XCER_DEFAULT,
+
+        MCBSP_FMKS(PCR, XIOEN, SP)              | //
+        MCBSP_FMKS(PCR, RIOEN, SP)              | //
+        MCBSP_FMKS(PCR, FSXM, INTERNAL)         | //
+        MCBSP_FMKS(PCR, FSRM, EXTERNAL)         |
+        MCBSP_FMKS(PCR, CLKXM, OUTPUT)          | // Transmitter clock mode bit (OUTPUT (1) = Data clock CLKX driven by the internal sample generator)
+        MCBSP_FMKS(PCR, CLKRM, INPUT)           | // Receiver clock mode bit (INPUT (0) = Data clock CLKR is an input)
+        MCBSP_FMKS(PCR, CLKSSTAT, DEFAULT)      |
+        MCBSP_FMKS(PCR, DXSTAT, DEFAULT)        |
+        MCBSP_FMKS(PCR, FSXP, ACTIVELOW)        | //
+        MCBSP_FMKS(PCR, FSRP, ACTIVELOW)        | //
+        MCBSP_FMKS(PCR, CLKXP, RISING)          | //
+        MCBSP_FMKS(PCR, CLKRP, RISING)            //
+    };*/
+
+/*hMcbsp = MCBSP_open(MCBSP_DEV0,MCBSP_OPEN_RESET);
+
+MCBSP_config(hMcbsp,&MCBSP0_SPI_Cfg_Init);*/
 
 
 
