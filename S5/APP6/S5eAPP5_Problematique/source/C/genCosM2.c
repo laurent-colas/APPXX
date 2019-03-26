7a60c818f6ac59bfa8aff39fe2f7ea8166fd44fe
berthiegouin <berthie.gouin-ferland@usherbrooke.ca>
2019-03-26 11:32:43 -0400

    chagement calcul avec cos pour constantes

--- S5/APP6/S5eAPP5_Problematique/source/C/genCosM2.c
+++ S5/APP6/S5eAPP5_Problematique/source/C/genCosM2.c
@@ -30,19 +30,19 @@ FILE * file;
 
 void initGenM2(){
     //créaction unique des pas pour chaque corde
-    C_delta[0].real = cos(2*PI*82.407/fe);
-    C_delta[1].real = cos(2*PI*110.000/fe);
-    C_delta[2].real = cos(2*PI*146.832/fe);
-    C_delta[3].real = cos(2*PI*195.998/fe);
-    C_delta[4].real = cos(2*PI*246.942/fe);
-    C_delta[5].real = cos(2*PI*329.628/fe);
+    C_delta[0].real = 0.997906242568737;
+    C_delta[1].real = 0.996270376492941;
+    C_delta[2].real = 0.993357832349561;
+    C_delta[3].real = 0.988175159960493;
+    C_delta[4].real = 0.981251008886904;
+    C_delta[5].real = 0.966674940343783;
 
-    C_delta[0].img = sin(2*PI*82.407/fe);
-    C_delta[1].img = sin(2*PI*110.000/fe);
-    C_delta[2].img = sin(2*PI*146.832/fe);
-    C_delta[3].img = sin(2*PI*195.998/fe);
-    C_delta[4].img = sin(2*PI*246.942/fe);
-    C_delta[5].img = sin(2*PI*329.628/fe);
+    C_delta[0].img = 0.064677129206116;
+    C_delta[1].img = 0.086286365797923;
+    C_delta[2].img = 0.115066141456906;
+    C_delta[3].img = 0.153329231515238;
+    C_delta[4].img = 0.192734162925087;
+    C_delta[5].img = 0.256006952466812;
 
     // créaction du signal et écriture dans un fichier "CosM2.dat"
     int m = 0;
