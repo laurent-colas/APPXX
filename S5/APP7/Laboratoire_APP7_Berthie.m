cc0971a14b0cabff22975b6e23c639349db71ac2
berthiegouin <berthie.gouin-ferland@usherbrooke.ca>
2019-04-03 12:53:24 -0400

    lab 1

---
+++ S5/APP7/Laboratoire_APP7_Berthie.m
@@ -0,0 +1,15 @@
+
+%% LABORATOIRE 1 - APP7 %%
+
+%%Clear
+clearvars
+close all 
+clc
+
+%% Problème 1 – Analyse de filtre FIR
+
+% a)
+
+% b)
+
+%% Problème 2 - Conception de filtre FIR par équations de transformation
