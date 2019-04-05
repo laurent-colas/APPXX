578353f3cd161a0743041c2dd2726e0aa35e0af9
berthiegouin <berthie.gouin-ferland@usherbrooke.ca>
2019-04-05 14:39:24 -0400

    la partie d'identification de la réponse impultionelle est fini est optimisée

--- S5/APP7/goub2904-coll2903-gauj2828.m
+++ S5/APP7/goub2904-coll2903-gauj2828.m
@@ -22,23 +22,36 @@ A = toeplitz(Rxx(m+1:end));             % Création de la matrice de Toeplitz A
 b = flip(Rxy(1:m+1));                   % Création de la matrice b
 
 
-
 %% Optimisation
 h_opt = linsolve(A(1:m,1:m),b(1:m));  
 bruit_estime = filter(h_opt,[1,0],bruit_emis);
 err = sum((bruit_estime - bruit_capte).^2);
 %%
-i = m-10;
+pas = 1000;
+i = m-pas;
 while err > 0.5
 h_opt = linsolve(A(1:i,1:i),b(1:i));  
 bruit_estime = filter(h_opt,[1,0],bruit_emis);
 err = sum((bruit_estime - bruit_capte).^2)
-i = i-10
+
+    if err < 0.5
+        if pas == 1
+            break;
+        else
+        i = i+pas;
+        err = 1;
+        pas = pas/10;
+        end
+    end
+i = i-pas
 end
+
 
 %%
 figure
-plot(h_bert)
+plot(h_bert(1:numel(h_opt)))
 hold on 
 plot(normal(h_opt))
+plot(abs(h_bert(1:numel(h_opt))-normal(h_opt)))
+
 
